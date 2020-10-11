e94466695147d6260d433c66f9bf18eab714f3e7
divyashree801 <divya.testbb@gmail.com>
2020-10-11 15:54:43 +0530

    modified program with while loop

--- lib/src/main/java/com/example/lib/TenNaturualNumbers.java
+++ lib/src/main/java/com/example/lib/TenNaturualNumbers.java
@@ -3,54 +3,16 @@ package com.example.lib;
 import java.util.Scanner;
 
 public class TenNaturualNumbers {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         System.out.println("How many natural number you want to print");
         int num = 10;
-        int count=0;
-        System.out.println("Here are your "+num+" numbers");
-        if(count<10)
+        int count = 1;
+        System.out.println("Here are your " + num + " numbers");
+        while(count<=10)
         {
-            System.out.println(++count);
+            System.out.println(count);
+            count++;
         }
-        if(count<10)
-        {
-            System.out.println(++count);
-        }
-        if(count<10)
-        {
-            System.out.println(++count);
-        }
-        if(count<10)
-        {
-            System.out.println(++count);
-        }
-        if(count<10)
-        {
-            System.out.println(++count);
-        }
-        if(count<10)
-        {
-            System.out.println(++count);
-        }
-        if(count<10)
-        {
-            System.out.println(++count);
-        }
-        if(count<10)
-        {
-            System.out.println(++count);
-        }
-        if(count<10)
-        {
-            System.out.println(++count);
-        }
-        if(count<10)
-        {
-            System.out.println(++count);
-        }
-
     }
-
 
 }
